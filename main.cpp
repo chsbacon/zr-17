753a19818c2af8603590457d8f00db684353e9e4
jonahweissman <jonahrweissman@gmail.com>
2017-09-28 19:26:35 -0400

    Add unhelpful comment

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 void init(){
-	//This function is called once when your code is first loaded.
-
-	//IMPORTANT: make sure to set any variables that need an initial value.
-	//Do not assume variables will be set to 0 automatically!
 	DEBUG(("BACON"));
 	DEBUG(("Another Debug!"));
+	// more stuff
 }
 
 void loop(){
