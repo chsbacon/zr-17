e62b478430c16048fe81c31568d135def4ab312a
asaunde78 <32373828+asaunde78@users.noreply.github.com>
2017-10-11 22:13:22 -0400

    looking in the xy plane fixed attitude problem (forces to look in xy plane) and added more comments. Still needs to simplify and cut down on code size

--- main.cpp
+++ main.cpp
@@ -39,17 +39,13 @@ bool drillUp;
 bool keepSearching;
 float drillSide;
 float spinVec[3];
-float xVec[3];
 int difDrill[2];
+float endDrillVec[3];
 
 void init(){
     spinVec[0] = 0.0f;
     spinVec[1] = 0.0f;
     spinVec[2] = 0.4f;
-    
-    xVec[0] = 1;
-    xVec[1] = 0;
-    xVec[2] = 0;
     
     vcoef=.154;//A coefficient for our movement speed
     // zeroVec[0]=zeroVec[1]=zeroVec[2]=0;
@@ -67,73 +63,65 @@ void loop(){
     api.getMyZRState(myState);
     api.getOtherZRState(enState);//Makes sure our data on where they are is up to date
     if(moveDrill(difDrill)) {
-        difDrill[0]++;
+        difDrill[0]--;
+        difDrill[1]--;
     }
     moveDrill(difDrill);
-    
-    
-    
 	//This function is called once per second.  Use it to control the satellite.
 }
 
 bool moveDrill(int drillSquare[2]) {
     float drillLoc[3];
-    float endDrillVec[3];
     float drillStartVec[3];
     float angleToComplete;
     //float angleToComplete;
-    
-    memcpy(drillStartVec, myAtt, 12);
-    drillStartVec[2] = 0;
     game.square2pos(drillSquare, drillLoc);
     drillLoc[2] = .52;
-    mathVecCross(endDrillVec, spinVec, drillStartVec);
-    //api.setAttitudeTarget(xVec);
+    memcpy(tempVec, myAtt, 12);
+    tempVec[2] = 0;
         
     if((game.getDrills(drillLoc) < 3 && !game.checkSample() == true)^(game.checkSample() == true)) {
         api.setPositionTarget(drillLoc);
         api.setAttRateTarget(zeroVec);
-        
         if(dist(myPos, drillLoc) < .01) {
             api.setVelocityTarget(zeroVec);
-            
-           
-            if(mathVecMagnitude(myVel, 3) < .01) {
+            api.setAttitudeTarget(tempVec);  
+            if(mathVecMagnitude(myVel, 3) < .01 && angle(tempVec, myAtt) < .19634f) {
+                //checks to make sure the sphere is stopped and looking within the right constraints 
                 if(game.getDrillEnabled() == false && dist(myRot, zeroVec) < .01f) {
-                    game.startDrill();
-                    
-                    DEBUG(("Drilling..."));
                     //if the drill is off and our sphere is not moving, start drilling
+                    game.startDrill();
+                    /*memcpy(drillStartVec, myAtt, 12);
+                    drillStartVec[2] = 0;
+                    mathVecCross(endDrillVec, spinVec, drillStartVec);*/
+                    DEBUG(("Drilling..."));
                 }
                 if(game.getDrillEnabled() == true) {
-                    angleToComplete = ((2*3.1415) - angle(myAtt, endDrillVec));
+                    // if the drill is on, spin!!!
+                    //angleToComplete = ((2*3.1415) - angle(myAtt, endDrillVec));
                     spinVec[0] = 0;
                     //spinVec[2] = 2*sin((1/3)*angleToComplete);
                     spinVec[1] = 0;
                     spinVec[2] = .4;
                     api.setAttRateTarget(spinVec);
-                    DEBUG(("%f", (180/3.1415)*angleToComplete));
+                    //DEBUG(("%f", (180/3.1415)*angleToComplete));
                     DEBUG(("Spinning..."));
                 }
                 if(game.isGeyserHere(drillSquare)) {
+                    //as soon as you find a geyser, turn off the drill
                     game.stopDrill();
                     return false;
                 }
                 if(game.checkSample() == true) {
+                    // see a sample, pick it up
                     game.pickupSample();
                     DEBUG(("Picking up..."));
                     if(game.getNumSamplesHeld() == 3) {
+                        //at 3 samples, stop drilling, this is subject to change
                         game.stopDrill();
                         DEBUG(("Stopping Drill..."));
                         return true;
                     }
-                    //one the sample is picked up, it checks inventory to see if there is more to get 
-                    /*if(game.getNumSamplesHeld() < 3) {
-                        state -= 1.0f;
-                    }
-                    else {
-                        state += 1.0f;
-                    }*/
                 }
             }
         }
