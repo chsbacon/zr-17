cd8f46ff12a07158d79b1eb27f3d201378bf8ccc
Mleblang <32373827+Mleblang@users.noreply.github.com>
2017-10-19 16:15:57 -0400

    Adding better test positioning better test positioning for better testing and tweaking

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ void init(){
     
     fueltosquare = 5; //how fast the sphere moves between squares (base speed)
     
-    testPosition[0] = .6;
-    testPosition[1] = .4;
-    testPosition[2] = .3;
+    testPosition[0] = 0.5f;
+    testPosition[1] = -0.5f;
+    testPosition[2] = 0.5f;
 }
 
 void loop(){
@@ -26,9 +26,21 @@ void loop(){
 	//timeLeft < 180 - time2base
 	
 	
-	if(myFuel <= 100 - FuelToGoBTB(myState, fueltosquare, origin)){
+	if(myFuel <= FuelToGoBTB(myState, fueltosquare, origin)){
 	    DEBUG(("Going back to Base station"));
 	    api.setPositionTarget(origin);
+	}
+	
+	else{
+	    if(myState[0] <= 0.6 && myState[0] >= 0.4){
+	        testPosition[0] = -0.5;
+	    }
+	
+	    //going to (0.5, -0.5, 0.5)
+	    if(myState[0] <= -0.4 && myState[0] >= -0.6){
+	        testPosition[0] = 0.5;
+    	}
+    	
 	}
 	
 }
@@ -40,7 +52,7 @@ float FuelToGoBTB(float myPos[12], float fuel2square, float baseStation[3]){
     DEBUG(("Magnitude is %f", magnitude));
     fuel2square = (fuel2square/2)*(magnitude*10);
     float fuel2base= magnitude*fuel2square; //finding the time it'll take to get back to the Base Station
-    fuel2base = 180-fuel2base;
+    fuel2base = 100-fuel2base;
     DEBUG(("The time to get back to the base is %f",fuel2base));
     
     return fuel2base;
