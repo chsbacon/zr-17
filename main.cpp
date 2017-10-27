e29c6f2480e57660494b3a0b805096729ad125e9
jtnachbar <nachbarjamie@gmail.com>
2017-10-26 23:30:35 -0400

    guards at the end instead DOES NOT MEET CODESIZE
    Meant largely for testing anti-guard behavior against. Guard logic is on line 205.

--- main.cpp
+++ main.cpp
@@ -16,36 +16,35 @@ float enState[12];
 #define enVel (&enState[3])
 #define enAtt (&enState[6])
 #define enRot (&enState[9])//These are pointers. They will have the values that
+#define TWODROPS false
 #define MAXDRILLS 3
 int siteCoords[3];
 bool newLoc;
-
-//could improve these flags to save codesize
 bool dropping;
 bool drilling;
-bool guarding;
 bool twoDrops;
+bool guarding;
 //are described in their names, and act as length-3 float arrays
 int samples;
 float vcoef;
-
 void init(){
     newLoc=true;
     // zeroVec[0]=zeroVec[1]=zeroVec[2]=0;
 	memset(zeroVec, 0.0f, 12);//Sets all places in an array to 0
-	#define SPEEDCONST .5f
-    #define DERIVCONST 2.5f
+	#define SPEEDCONST .35f
+    #define DERIVCONST 2.35f
     api.setPosGains(SPEEDCONST,0,DERIVCONST);
     //api.setAttGains(0.7f,0.1f,3.f);
     //api.setAttGains(0.f,0.f,0.f);
     dropping=false;
     samples=0;
-	drilling=false; 
+	drilling=false;
 	twoDrops=false;
 	guarding=false;
 }
 
 void loop(){
+    
     
     if (game.checkSample()){
         game.dropSample(4);
@@ -57,13 +56,7 @@ void loop(){
             game.dropSample(i);
         }
         dropping=false;
-        samples=-100;
     }
-    
-    //calculate guardSpot
-    memcpy(guardPos, enPos, 12);
-    scale(guardPos, 0.16f/mathVecMagnitude(enPos, 3));
-    
     api.getMySphState(myState);
     float myAtt[3];
     zeroVec[0]-=1;
@@ -74,14 +67,7 @@ void loop(){
     game.square2pos(mySquare,usefulVec);
     bool geyserOnMe;
     geyserOnMe=game.isGeyserHere(mySquare);
-    if (game.getScore()<7 and geyserOnMe){
-        twoDrops=true;
-    }
-    //What?
     twoDrops=true;
-    if(geyserOnMe and game.getNumSamplesHeld() > 4){
-        dropping=true;
-    }
     float maxDist=100;//Sets this large
     float modPos[3];
     memcpy(modPos,myPos,12);
@@ -89,7 +75,7 @@ void loop(){
         modPos[i]+=(myPos[i]-usefulVec[i])*6*geyserOnMe;
     }    
         
-    if (newLoc and !game.checkSample() and not drilling and not guarding){
+    if (newLoc and !game.checkSample() and not drilling){
         DEBUG(("%d",newLoc));
         DEBUG(("reselecting"));
         for (int i=-8;i<9;i++){//This checks all of the grid spaces, and sees which is both
@@ -102,10 +88,11 @@ void loop(){
                     usefulVec[2]=0.35f;
                     float score=dist(usefulVec,modPos);
                     if (score<maxDist 
-                    and game.getDrills(usefulIntVec)<MAXDRILLS 
+                    and game.getDrills(usefulIntVec)<1 
                     and not game.isGeyserHere(usefulIntVec) 
-                    and i*i+j*j>8 
-                    and i%2+j%2<=1){
+                    and i*i+j*j>8 and i*i<16 and j*j<16 
+                    
+                    and dist(enPos,usefulVec)>.22f){
                         siteCoords[0]=i;siteCoords[1]=j;
                         //DEBUG(("Changed %f", score));
                         maxDist = score;
@@ -119,24 +106,20 @@ void loop(){
     // if (geyserOnMe){
     //     vcoef+=.04f;
     // }
-    if (game.getNumSamplesHeld()>2 and ((api.getTime()==160) or (game.getFuelRemaining() < .13f and game.getFuelRemaining() > .10f))){
-        dropping=true;
-        drilling=false;
-        game.stopDrill();
-    }
+    
     
     float rotConst;
     rotConst=0;
     
-    //drill if we have less than 5 samples and we either have enough fuel or we're close to the surface and don't have many samples already
-    if ((((samples%3>0 or samples<0)and twoDrops) or game.getNumSamplesHeld()<5) and not dropping and not guarding){//Second to last condition is redundant
+    //drill if we have less than 5 samples and we either have enough fuel or we're close to the surface and don't have many samples already, drill
+    if (not dropping){//Second to last condition is redundant
         DEBUG(("%i %i", siteCoords[0],siteCoords[1]));
         DEBUG(("%i %i", mySquare[0],mySquare[1]));
         game.square2pos(siteCoords,positionTarget);
         //adjust positiontarget to the corner of a square
         for (int i=0;i<2;i++){
             //positionTarget[i]+=0.033f*(siteCoords[i]>0?1:-1)*(siteCoords[i]%2>0?1:-1)*(geyserOnMe?1:-1);//can use xor for codesize
-            positionTarget[i]+=0.034f*((siteCoords[i]>0)^(siteCoords[i]%2>0)^(geyserOnMe)?-.5f:1);
+            positionTarget[i]+=0.032f*((siteCoords[i]>0)^(siteCoords[i]!=2)^(geyserOnMe)?1:-1)*(1-(siteCoords[i]*siteCoords[i]==1)*.2f);
         }
         //set this to go to the surface
         positionTarget[2]=0.35f;
@@ -150,9 +133,6 @@ void loop(){
             usefulVec[0]=-myAtt[1];usefulVec[1]=myAtt[0];usefulVec[2]=myAtt[2]*-5;
             api.setAttitudeTarget(usefulVec);
             
-            
-            // scale(usefulVec,.2f/mathVecMagnitude(usefulVec,3));
-            // api.setAttRateTarget(usefulVec);
             // usefulVec[0]=0;
             // usefulVec[1]=0;
             // usefulVec[2]=0;
@@ -161,7 +141,7 @@ void loop(){
                 game.startDrill();
             }
             else{
-                rotConst=.3f;
+                rotConst=.1f;
             }
             drilling=true;
             
@@ -174,82 +154,78 @@ void loop(){
             // api.setAttRateTarget(usefulVec);
             DEBUG(("Slowing"));
             drilling=false;
-            rotConst=mathVecMagnitude(myRot,3)*-.2f;
+            rotConst=-.1f;
         }
-        PRINTVEC("myQuatAtt",myQuatAtt);
-        myQuatAtt[3]*=-1;//inverts rotation - now rotates fundamental basis rotation vector (k-hat) to our basis
-        zeroVec[2]=rotConst;
-        api.quat2AttVec(zeroVec,myQuatAtt,usefulVec);
-        zeroVec[2]=0;
-        if (drilling){
-            api.setAttRateTarget(usefulVec);
-        }
+        
         
        
     }
     //otherwise, drop off our samples
     else{
-        if(guarding){
-            memcpy(positionTarget, guardPos, 12);
-        }
-        else{
         memcpy(positionTarget,myPos,12);
-        scale(positionTarget,0.23f/mathVecMagnitude(myPos,3));
-        }
+        scale(positionTarget,.23f/mathVecMagnitude(myPos,3));
         zeroVec[2]-=1;
         api.setAttitudeTarget(zeroVec);
         zeroVec[2]+=1;
-        api.setAttRateTarget(zeroVec);
+        rotConst=.1f;
+    }
+    //PRINTVEC("myQuatAtt",myQuatAtt);
+    myQuatAtt[3]*=-1;//inverts rotation - now rotates fundamental basis rotation vector (k-hat) to our basis
+    zeroVec[2]=rotConst;
+    api.quat2AttVec(zeroVec,myQuatAtt,usefulVec);
+    zeroVec[2]=0;
+    if (drilling){
+        api.setAttRateTarget(usefulVec);
     }
     //if our drill breaks or we get a geyser, stop the current drill
     if (game.getDrillError() 
-    //or (mySquare[0]!=siteCoords[0] or mySquare[1]!=siteCoords[1]) 
-    //or mathVecMagnitude(myVel,3)>0.009f
-    or (samples==6
     or geyserOnMe 
-    or game.getDrills(mySquare)>MAXDRILLS-1)){
-        if (geyserOnMe and drilling){
-            samples-=samples%3;
+    or game.getDrills(mySquare)>MAXDRILLS-1){
+        if (TWODROPS and samples>4){
+            dropping=true;
+            samples=-100;
+        }
+        if (!TWODROPS and game.getNumSamplesHeld()>3){
+            dropping=true;
         }
         game.stopDrill();
         newLoc=true;
         drilling=false;
     }
-    
-    //if we're closer than them to the guarding position and we're ahead
-    if((dist(enPos, guardPos) - dist(myPos, guardPos) > .15) and (game.getScore() - game.getOtherScore() > 2)){
-        guarding=true;
-    }
-    //if we were guarding and they're too close to the surface
-    if(guarding and enPos[2] > .28){
-        guarding=false;
+    if (game.getNumSamplesHeld()>2 and ((api.getTime()>157 and api.getTime()<163) or (game.getFuelRemaining() < .16f and game.getFuelRemaining() > .8f))){
+        dropping=true;
+        drilling=false;
+        game.stopDrill();
     }
     
-    DEBUG(("Guarding: %d", guarding));
-
+    memcpy(guardPos, enPos, 12);
+    scale(guardPos, .1/mathVecMagnitude(enPos, 3));
+    
+    if((game.getScore() > 36 and game.getScore() - game.getOtherScore() >= 15 and dist(myPos, zeroVec) < .24) or guarding){
+        memcpy(positionTarget, guardPos, 12);
+        guarding = true;
+        DEBUG(("Guarding"));
+    }
     
     
 	#define destination positionTarget//This (next 20 or so lines) is movement code.
 	//It is fairly strange - we will go over exactly how it works eventually
     float distance,flocal,fvector[3];
-    #define ACCEL .0135f
-    mathVecSubtract(fvector, destination, myPos, 3);//Gets the vector from us to the target
+    #define ACCEL .014f
+    //mathVecSubtract(fvector, destination, myPos, 3);//Gets the vector from us to the target
     distance = mathVecNormalize(fvector, 3);
-    if (distance > 0.05f) {//If not close, decide on our velocity
-        flocal = vcoef;
-        if (flocal*flocal/ACCEL>distance-.02f){//Cap on how fast we go
-            flocal = sqrtf(distance*ACCEL)-.02f;
-            //DEBUG(("Slower"));
-        }
-        else if (!geyserOnMe and flocal>ACCEL+mathVecMagnitude(myVel,3)){
-            flocal=ACCEL+mathVecMagnitude(myVel,3);
-        }
-        scale(fvector, flocal);
-        api.setVelocityTarget(fvector);
+    mathVecSubtract(fvector, destination, myPos, 3);
+    
+    scale(myVel,.26f);
+    mathVecSubtract(fvector,fvector,myVel,3);
+    scale(fvector,.24f);
+    if (geyserOnMe){
+        flocal=mathVecMagnitude(fvector,3)/.2f;
+        fvector[0]/=flocal;
+        fvector[1]/=flocal;
+        fvector[2]=0.01f;
     }
-    else{//If close:
-        api.setPositionTarget(destination);
-    }
+    api.setVelocityTarget(fvector);
 }
 
 float dist(float* vec1, float* vec2) {
