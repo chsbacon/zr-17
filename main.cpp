1b08a9da64297712ae8d1acbe905ff15974c00cb
MeisterNewman <stephenharmonnewman@gmail.com>
2017-11-28 11:31:12 -0500

    Functionality, codesize Now moderately consistent Codesize reduction

--- main.cpp
+++ main.cpp
@@ -50,8 +50,6 @@ void loop(){
     int mySquare[3];
     float enState[12];
     memset(zeroVec, 0.0f, 12);//Sets all places in an array to 0
-    float enDeltaScore=game.getOtherScore()-enScore;
-    enScore=enScore+enDeltaScore;
     if (game.checkSample()){
         game.dropSample(4);
         samples+=(bool)(game.pickupSample());
@@ -75,12 +73,6 @@ void loop(){
     geyserOnMe=game.isGeyserHere(mySquare);
     float maxDist=100;//Sets this large
     
-    if (enDeltaScore==3.5f){
-        game.pos2square(enPos,siteCoords);
-        siteCoords[0]*=-1;
-        siteCoords[1]*=-1;
-    }
-    
     bool onSite=(mySquare[0]==siteCoords[0] and mySquare[1]==siteCoords[1]);
     if (!game.hasAnalyzer()){
         positionTarget[0]=.3f;
@@ -95,45 +87,52 @@ void loop(){
         DEBUG(("%f",terrain));
         if (terrain>.9f){
             game.pos2square(myPos,tenCoords);
+            tenFound=true;
         }
-        
         else{
+            valArray[mySquare[0]+6-(mySquare[0]>0)][mySquare[1]-1]=false;
             if (myPos[1]<0){
                 mySquare[0]*=-1;
                 mySquare[1]*=-1;
             }
             int valCount=0;
-            siteCoords[0]=0;siteCoords[1]=0;
+            memset(siteCoords,0,8);
             for (int i=-6;i<7;i++){
-                if (!i){
-                    i++;
-                }
-                for (int j=1;j<9;j++){
-                    bool inRad=(i-mySquare[0])*(i-mySquare[0])+(j-mySquare[1])*(j-mySquare[1])<(terrain>.5f?3:8);
-                    if (terrain>.15f){
-                        concFound=true;
-                        inRad=!inRad;
-                        DEBUG(("conc!"));
-                    }
-                    if (inRad){
-                        valArray[i+6][j-1]=false;
-                        if (i==-6 and j==1){
-                            DEBUG(("HI"));
+                if (i){
+                    for (int j=1;j<9;j++){
+                        usefulIntVec[0]=i;usefulIntVec[1]=j;
+                        int sqDist=intDist(usefulIntVec,mySquare);
+                        usefulIntVec[0]*=-1;
+                        usefulIntVec[1]*=-1;
+                        int sqDist2=intDist(usefulIntVec,mySquare);
+                        if (sqDist>sqDist2){
+                            sqDist=sqDist2;
                         }
-                    }
-                    if (valArray[i+6][j-1]){
-                        siteCoords[0]+=i;
-                        siteCoords[1]+=j;
-                        valCount++;
+                        bool inRad=sqDist<=(terrain>.5f?3:7);
+                        if (terrain>.15f){
+                            concFound=true;
+                            inRad=!inRad;
+                        }
+                        if (inRad){
+                            valArray[i+6-(i>0)][j-1]=false;
+                        }
+                        if (valArray[i+6-(i>0)][j-1]){
+                            siteCoords[0]+=i;
+                            siteCoords[1]+=j;
+                            valCount++;
+                        }
                     }
                 }
             }
-            siteCoords[0]+=(siteCoords[0]-mySquare[0]*valCount)*3;
-            siteCoords[1]+=(siteCoords[1]-mySquare[1]*valCount)*3;
+            //siteCoords[0]+=(siteCoords[0]-mySquare[0]*valCount);
+            //siteCoords[1]+=(siteCoords[1]-mySquare[1]*valCount);
             siteCoords[0]/=valCount;siteCoords[1]/=valCount;
+            if (!siteCoords[0]){
+                siteCoords[0]++;
+            }
             if (!concFound){
                 if (valArray[0][0]){
-                    siteCoords[0]=-4;
+                    siteCoords[0]=-5;
                     siteCoords[1]=2;
                 }
                 else if (valArray[11][0]){
@@ -145,15 +144,23 @@ void loop(){
                     siteCoords[1]=7;
                 }
             }
-            if (myPos[0]==siteCoords[0] and myPos[1]==siteCoords[1]){
+            while (!valArray[siteCoords[0]+6-(siteCoords[0]>0)][siteCoords[1]-1]){
                 siteCoords[0]+=1;
-                siteCoords[1]+=1;
+                if (siteCoords[0]>6){
+                    siteCoords[0]=-6;
+                    siteCoords[1]++;
+                }
+                if (!siteCoords[0]){
+                    siteCoords[0]++;
+                }
+                if (siteCoords[1]>8){
+                    siteCoords[1]=1;
+                }
             }
             if (myPos[1]<0){
-                siteCoords[0]*=-1;
                 siteCoords[1]*=-1;
+                siteCoords[0]*=-1;
             }
-            
             game.square2pos(siteCoords,positionTarget);
             DEBUG(("%i %i", siteCoords[0],siteCoords[1]));
             positionTarget[2]=.27f;
@@ -162,14 +169,37 @@ void loop(){
     }
     //drill if we have less than 5 samples and we either have enough fuel or we're close to the surface and don't have many samples already, drill
     else if ((not dropping)){
+        float maxDist=1000;
+        for (int i=-6;i<7;i++){
+            if (i){
+                for (int j=-8;j<9;j++){
+                    if (j){
+                        usefulIntVec[0]=i;usefulIntVec[1]=j;
+                        if (((drilling and mySquare[0]==usefulIntVec[0] and mySquare[1]==usefulIntVec[1]) or !game.getDrills(usefulIntVec))){
+                            game.square2pos(usefulIntVec,usefulVec);
+                            usefulVec[2]=game.getTerrainHeight(usefulIntVec);
+                            if (usefulIntVec[1]<0){
+                                usefulIntVec[0]*=-1;
+                                usefulIntVec[1]*=-1;
+                            }
+                            flocal=dist(usefulVec,myPos)*3+intDist(usefulIntVec,tenCoords)+.05f*(usefulVec[2]>=game.getTerrainHeight(myPos));
+                            if (flocal<maxDist and dist(enPos,usefulVec)>.3f){
+                                memcpy(positionTarget,usefulVec,12);
+                                maxDist=flocal;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         //adjust positiontarget to the corner of a square
-        game.square2pos(siteCoords,positionTarget);
-        positionTarget[0]+=((corner%2)*-2+1)*0.029f;
-        positionTarget[1]+=((corner/2)*-2+1)*0.029f;
+        game.pos2square(positionTarget,siteCoords);
+        positionTarget[0]+=(positionTarget[0]<0)*0.029f;
+        positionTarget[1]+=(positionTarget[1]<0)*0.029f;
         
         
         //positionTarget[2]=myPos[2];//vertical movement to avoid terrain
-        if (!onSite and (game.getTerrainHeight(mySquare)>game.getTerrainHeight(siteCoords) or dist(myPos,positionTarget)>.05f)){
+        if (!onSite and (game.getTerrainHeight(mySquare)<game.getTerrainHeight(siteCoords))){
             positionTarget[2]=.27f;
             DEBUG(("O"));
             if (myPos[2]>.29f){
@@ -191,7 +221,7 @@ void loop(){
         and (onSite))
         and not game.getDrillError()
         and (myPos[2]-positionTarget[2]<.02f and myPos[2]-positionTarget[2]>-0.02f)){
-            usefulVec[0]=-myAtt[1];usefulVec[1]=myAtt[0];usefulVec[2]=myAtt[2]*-5;
+            usefulVec[0]=-myAtt[1];usefulVec[1]=myAtt[0];usefulVec[2]=0;//myAtt[2]*-5;
             
             // usefulVec[0]=0;
             // usefulVec[1]=0;
@@ -200,7 +230,7 @@ void loop(){
             if (!game.getDrillEnabled()){
                 game.startDrill();
             }
-            zeroVec[2]=.04f;
+            //zeroVec[2]=.04f;
             drilling=true;
             
         }
@@ -212,7 +242,7 @@ void loop(){
             // api.setAttRateTarget(usefulVec);
             DEBUG(("Slowing"));
             drilling=false;
-            zeroVec[2]=-.04f;
+            //zeroVec[2]=-.04f;
         }
         api.setAttitudeTarget(usefulVec);
         
@@ -227,7 +257,7 @@ void loop(){
         }
         else{
             //maybe take out the mathvecMagnitude expression for codesize
-            scale(positionTarget,(.23f)/mathVecMagnitude(positionTarget,3));//go to a position that is .09 in the same direction at the enemy. In other words, between them and the origin.        }
+            scale(positionTarget,(.23f)/mathVecMagnitude(positionTarget,3));//go to a position that is .09 in the same direction at the enemy. In other words, between them and the origin.
         }
         zeroVec[2]-=1;
         api.setAttitudeTarget(zeroVec);
@@ -235,12 +265,13 @@ void loop(){
         
         
     }
-    PRINTVEC("myQuatAtt",myQuatAtt);
-    myQuatAtt[3]*=-1;//inverts rotation - now rotates fundamental basis rotation vector (k-hat) to our basis
-    api.quat2AttVec(zeroVec,myQuatAtt,usefulVec);
-    if (drilling){
-        api.setAttRateTarget(usefulVec);
-    }
+    
+    //PRINTVEC("myQuatAtt",myQuatAtt);
+    // myQuatAtt[3]*=-1;//inverts rotation - now rotates fundamental basis rotation vector (k-hat) to our basis
+    // api.quat2AttVec(zeroVec,myQuatAtt,usefulVec);
+    // if (drilling){
+    //     api.setAttRateTarget(usefulVec);
+    // }
     //if our drill breaks or we get a geyser, stop the current drill
     flocal=game.getFuelRemaining();
     if (game.getDrillError() 
@@ -283,18 +314,17 @@ void loop(){
     flocal=0.05f/(.05f+mathVecMagnitude(fvector,3));//Just storing this value as a functional boolean
     scale(myVel,.2f+flocal);
     mathVecSubtract(fvector,fvector,myVel,3);
-    scale(fvector,.27f-.09f*flocal);
+    scale(fvector,.2f-.09f*flocal);
     if (geyserOnMe){
         fvector[2]=0;
         // flocal=mathVecMagnitude(fvector,3)/15;
         // fvector[0]/=flocal;
         // fvector[1]/=flocal;
-        scale(fvector,5/mathVecMagnitude(fvector,3));
+        scale(fvector,15/mathVecMagnitude(fvector,3));
         //fvector[2]=0.05f;
     }
     if (!tenFound and game.hasAnalyzer()){
         mathVecNormalize(fvector,3);
-        scale(fvector,.039f);
         fvector[2]=.1f*(.27f-myPos[2]);
         while (mathVecMagnitude(fvector,3)>.039f){
             scale(fvector,.99f);
@@ -319,4 +349,7 @@ void scale (float* vec, float scale) {//This function scales a length-3 vector b
     for (int i=0; i<3; i++) {
         vec[i] *= scale;
     }
+}
+int intDist(int a1[2],int a2[2]){
+    return ((a1[0]-a2[0])*(a1[0]-a2[0])+(a1[1]-a2[1])*(a1[1]-a2[1]));
 }
