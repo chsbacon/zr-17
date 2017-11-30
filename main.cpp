da2878fcea7ccc0a367f6e455046bce29a291c6b
MeisterNewman <stephenharmonnewman@gmail.com>
2017-11-30 09:54:40 -0500

    Farther Drills Now don't drill close to center in order to prevent issues with zone

--- main.cpp
+++ main.cpp
@@ -136,7 +136,7 @@ void loop() {
         for (int i = -6; i<6; i++){ // checks all clumps of 4 squares (blocks)
         // and sees which is both closest to us and in the center.
             for (int j = -8; j<8; j++){
-                if (i*j !=0 and (i < -3 or i > 2 or j < -3 or j > 2)) {
+                if (i*j !=0 and (i < -4 or i > 3 or j < -4 or j > 3)) {
                     
                     // initialize array to store number of squares at each level
                     int heights[4];
@@ -320,8 +320,8 @@ void loop() {
             #define BASE_STATION_RADIUS 0.24f
             // depending on whether or not we are guarding, there are two
             // different target distances from the origin
-            scale(positionTarget, ((BASE_STATION_RADIUS-0.01f) - (0.16f * guarding))
-                  / mathVecMagnitude(positionTarget, 3));
+            scale(positionTarget, (((BASE_STATION_RADIUS-0.1f) - (0.11f * guarding))
+                  / mathVecMagnitude(positionTarget, 3)));
         }
         // rotate to satisfy drop off requirement
         zeroVec[2] -= 1;
@@ -393,7 +393,7 @@ void loop() {
     flocal = 0.05f / (0.05f + mathVecMagnitude(fvector, 3));
     scale(myVel, 0.2f + flocal);
     mathVecSubtract(fvector, fvector, myVel, 3);
-    scale(fvector, 0.27f - (0.09f * flocal));
+    scale(fvector, 0.25f - (0.09f * flocal));
     
     // if we're on a geyser
     if (geyserOnMe) {
