1fa0d456248741cb10986bb74d4ef69fd9aac8ea
jonahweissman <19804455+jonahweissman@users.noreply.github.com>
2017-12-11 15:59:37 -0500

    Improve documentation

--- main.cpp
+++ main.cpp
@@ -139,7 +139,11 @@ void loop() {
             concArray[mySquare[0] + (TEN_ZONE_WIDTH/2) - (mySquare[0] > 0)]
                 [mySquare[1] - 1] = false;
             
+			// the number of possible tens
             int valCount = 0;
+			
+			// we need to initialize at zero because we are going to sum
+			// the square indices of all posible tens 
             memset(targetSquare, 0, 8);
             for (int i = -TEN_ZONE_WIDTH; i <= TEN_ZONE_WIDTH; i++) {
                 // don't check zero column
@@ -153,7 +157,7 @@ void loop() {
                         // the mirror of the square being evaluated
                         usefulIntVec[0] *= -1;
                         usefulIntVec[1] *= -1;
-                        int sqDist2 = intDist(usefulIntVec,mySquare);
+                        int sqDist2 = intDist(usefulIntVec, mySquare);
                         
                         if (sqDist > sqDist2){
                             sqDist = sqDist2;
@@ -163,90 +167,124 @@ void loop() {
                         // us and the closer of the square being examined and
                         // its mirror
                         
+						// is the square we are examining within the appropriate
+						// radius for it to be a ten?
                         bool inRadius = sqDist <= (concentration>0.5f ? 3 : 7);
+			    
                         // if we are over a 3 or a 6
                         if (concentration > 0.15f) {
-                            concFound=true;
-                            inRadius=!inRadius;
+                            concFound = true;
+                            inRadius = !inRadius;
                         }
-                        if (inRadius){
-                            concArray[i+6-(i>0)][j-1]=false;
+			
+						// At this point, inRadius is true if we think that
+						// the square we are examining could not be a ten
+			    
+                        if (inRadius) {
+			    			// rule out this square
+                            concArray[i + (TEN_ZONE_WIDTH/2) - (i > 0)][j - 1] = false;
                         }
-                        if (concArray[i+6-(i>0)][j-1]){
-                            targetSquare[0]+=i;
-                            targetSquare[1]+=j;
+						
+						// if the square we are examining is a possible ten
+                        if (concArray[i + (TEN_ZONE_WIDTH/2) - (i > 0)][j - 1]) {
+							// keep track of stuff necessary to calculate centroid
+                            targetSquare[0] += i;
+                            targetSquare[1] += j;
                             valCount++;
                         }
                     }
                 }
             }
-            //targetSquare[0]+=(targetSquare[0]-mySquare[0]*valCount);
-            //targetSquare[1]+=(targetSquare[1]-mySquare[1]*valCount);
-            targetSquare[0]/=valCount;targetSquare[1]/=valCount;
-            if (!targetSquare[0]){
+			
+			// calculate the centroid
+            targetSquare[0] /= valCount;
+			targetSquare[1] /= valCount;
+            
+			// account for case of average being zero
+			if (!targetSquare[0]) {
                 targetSquare[0]++;
             }
-            if (!concFound){
-                if (concArray[0][0]){
-                    targetSquare[0]=-5;
-                    targetSquare[1]=2;
+			
+			// this outlines the hard-coded search pattern
+            if (!concFound) {
+                if (concArray[0][0]) {
+                    targetSquare[0] = -5;
+                    targetSquare[1] = 2;
                 }
-                else if (concArray[11][0]){
-                    targetSquare[0]=4;
-                    targetSquare[1]=2;
+                else if (concArray[11][0]) {
+                    targetSquare[0] = 4;
+                    targetSquare[1] = 2;
                 }
-                else if (concArray[11][7]){
-                    targetSquare[0]=4;
-                    targetSquare[1]=7;
+                else if (concArray[11][7]) {
+                    targetSquare[0] = 4;
+                    targetSquare[1] = 7;
                 }
             }
-            while (!concArray[targetSquare[0]+6-(targetSquare[0]>-1)][targetSquare[1]-1]){
-                targetSquare[0]+=1;
-                if (targetSquare[0]>6){
-                    targetSquare[0]=-6;
+			
+			// change targetSquare until it we find a possible ten
+            while (!concArray[targetSquare[0] + (TEN_ZONE_WIDTH/2) - (targetSquare[0] > -1)]
+				   [targetSquare[1]-1]) {
+                targetSquare[0] += 1;
+				// horizontal wrap
+                if (targetSquare[0] > 6) {
+                    targetSquare[0] = -6;
                     targetSquare[1]++;
-                }//removing the x=0 skip here shouldn't break things because of how the boolean subtraction is structured in the while condition (ends up same as prev) but may be wrong
-                if (targetSquare[1]>8){
-                    targetSquare[1]=1;
                 }
+                // vertical wrap
+				if (targetSquare[1] > 8) {
+                    targetSquare[1] = 1;
+				}
             }
-            if (myPos[1]<0){
-                targetSquare[1]*=-1;
-                targetSquare[0]*=-1;
+			
+			// flip if necessary to get back to correct square
+            if (myPos[1] < 0){
+                targetSquare[1] *= -1;
+                targetSquare[0] *= -1;
             }
-            game.square2pos(targetSquare,positionTarget);
-            DEBUG(("%i %i", targetSquare[0],targetSquare[1]));
-            positionTarget[2]=0.f;
+			
+            game.square2pos(targetSquare, positionTarget);
+            DEBUG(("%i %i", targetSquare[0], targetSquare[1]));
+			// scan height
+            positionTarget[2] = 0.0f;
             
         }
     }
-    
-    //drill if we have less than 5 samples and we either have enough fuel or we're close to the surface and don't have many samples already, drill
-    else if ((not dropping)){
-        memcpy(nextSquare,mySquare,8);
-        float maxDist=1000;
-        for (int i=-6;i<7;i++){
-            if (i){
-                for (int j=-8;j<9;j++){
-                    if (j){
-                        usefulIntVec[0]=i;usefulIntVec[1]=j;
-                        if (((drilling and mySquare[0]==usefulIntVec[0] and mySquare[1]==usefulIntVec[1]) or !game.getDrills(usefulIntVec))){
-                            game.square2pos(usefulIntVec,usefulVec);
-                            usefulVec[2]=game.getTerrainHeight(usefulIntVec);
-                            // if (usefulIntVec[1]<0){
-                            //     usefulIntVec[0]*=-1;
-                            //     usefulIntVec[1]*=-1;
-                            // }
-                            flocal=dist(usefulVec,myPos)+intDist(usefulIntVec,tenCoords)-.1f*(usefulIntVec[2]>game.getTerrainHeight(mySquare));
-                            if (flocal<maxDist and dist(enPos,usefulVec)>.3f and !game.isGeyserHere(usefulIntVec)){
-                                if (drilling and (mySquare[0]!=usefulIntVec[0] or mySquare[1]!=usefulIntVec[1])){
-                                    memcpy(nextSquare,usefulIntVec,8);
-                                    maxDist=flocal;
+    else if (not dropping) {
+        memcpy(nextSquare, mySquare, 8);
+        float maxDist = 1000;
+        for (int i = -(TEN_ZONE_WIDTH/2); i<=(TEN_ZONE_WIDTH/2); i++) {
+			// ignore zero column
+            if (i) {
+                for (int j = -TEN_ZONE_HEIGHT; j <= TEN_ZONE_HEIGHT; j++) {
+                    if (j) {
+                        usefulIntVec[0] = i;
+						usefulIntVec[1] = j;
+						// if we're (drilling and on square that we are examining) or
+						// the square we are examining has never been drilled
+                        if (( (drilling and mySquare[0] == usefulIntVec[0] and mySquare[1] == usefulIntVec[1])
+							or !game.getDrills(usefulIntVec))) {
+                            // store the position of the square we are examining
+							game.square2pos(usefulIntVec, usefulVec);
+                            usefulVec[2] = game.getTerrainHeight(usefulIntVec);
+							
+							// how much dislike this square
+							// (we hate squares that are far from the ten)
+							flocal = dist(usefulVec, myPos)
+								+ intDist(usefulIntVec, tenCoords)
+								- 0.1f * (usefulIntVec[2] > game.getTerrainHeight(mySquare));
+							// conditions that must be met regardless of position
+                            if (flocal < maxDist
+								and dist(enPos, usefulVec) > 0.3f
+								and !game.isGeyserHere(usefulIntVec)) {
+								// if we are drilling at this spot already
+                                if (drilling and (mySquare[0] != usefulIntVec[0] or mySquare[1] != usefulIntVec[1])) {
+                                    memcpy(nextSquare, usefulIntVec, 8);
+                                    maxDist = flocal;
                                 }
-                                else{
-                                    memcpy(positionTarget,usefulVec,12);
-                                    if (!drilling){
-                                        maxDist=flocal;
+                                else {
+                                    memcpy(positionTarget, usefulVec, 12);
+                                    if (!drilling) {
+                                        maxDist = flocal;
                                     }
                                 }
                             }
@@ -255,160 +293,142 @@ void loop() {
                 }
             }
         }
-        if (game.getNumSamplesHeld()>2){
-            memset(nextSquare,0,8);
+		
+        if (game.getNumSamplesHeld() > 2) {
+            memset(nextSquare, 0, 8);
         }
-        //adjust positiontarget to the corner of a square
-        game.pos2square(positionTarget,targetSquare);
+		
+        game.pos2square(positionTarget, targetSquare);
         
-        
-        
-        //positionTarget[2]=myPos[2];//vertical movement to avoid terrain
-        if (!onSite){// and (game.getTerrainHeight(mySquare)<game.getTerrainHeight(targetSquare))){
-            positionTarget[2]=.27f;
+        if (!onSite) {
+            positionTarget[2] = 0.27f;
             DEBUG(("O"));
-            if (myPos[2]>.29f){
+            if (myPos[2] > 0.29f) {
                 DEBUG(("U"));
-                memcpy(positionTarget,myPos,8);
+				// do not move in the x and y direction if we are deep
+                memcpy(positionTarget, myPos, 8);
             }
         }
-        else{
-            DEBUG(("D"));
-            positionTarget[2]=game.getTerrainHeight(targetSquare)-.13f;
+        else {
+            DEBUG(("D")); // D is for Drill
+            positionTarget[2] = game.getTerrainHeight(targetSquare) - 0.13f;
         }
-        DEBUG(("%i %i", targetSquare[0],targetSquare[1]));
-        DEBUG(("%i %i", mySquare[0],mySquare[1]));
-        DEBUG(("%i %i", nextSquare[0],nextSquare[1]));
+        DEBUG(("%i %i", targetSquare[0], targetSquare[1]));
+        DEBUG(("%i %i", mySquare[0], mySquare[1]));
+        DEBUG(("%i %i", nextSquare[0], nextSquare[1]));
         
-        
-        //if we are on the right square and all the conditions line up, start spinning and drilling
-        if ((mathVecMagnitude(myVel,3)<.01f
-        and (mathVecMagnitude(myRot,3)<.035f or drilling)
-        and (onSite))
-        //and not game.getDrillError()
-        ){//and (myPos[2]-positionTarget[2]<0.02f and myPos[2]-positionTarget[2]>-0.02f)){
-            usefulVec[0]=-myAtt[1];usefulVec[1]=myAtt[0];usefulVec[2]=0;//myAtt[2]*-5;
+		// Drill code
+        if ((mathVecMagnitude(myVel, 3) < 0.01f
+        and (mathVecMagnitude(myRot, 3) < 0.035f or drilling)
+        and (onSite))) {
+            usefulVec[0] = -myAtt[1];
+			usefulVec[1] = myAtt[0];
+			usefulVec[2] = 0;
             
-            // usefulVec[0]=0;
-            // usefulVec[1]=0;
-            // usefulVec[2]=0;
-            // api.setAttRateTarget(usefulVec);
-            if (!game.getDrillEnabled()){
+            if (!game.getDrillEnabled()) {
                 game.startDrill();
             }
-            //zeroVec[2]=.04f;
-            drilling=true;
-            
+            drilling = true;
         }
-        else{
-            usefulVec[0]=myAtt[0];usefulVec[1]=myAtt[1];usefulVec[2]=0;
+        else {
+            usefulVec[0] = myAtt[0];
+			usefulVec[1] = myAtt[1];
+			usefulVec[2] = 0.0f;
             
-            // memcpy(usefulVec,myRot,12);
-            // scale(usefulVec,-0.6f);
-            // api.setAttRateTarget(usefulVec);
             DEBUG(("Slowing"));
-            drilling=false;
-            //zeroVec[2]=-.04f;
+            drilling = false;
         }
         api.setAttitudeTarget(usefulVec);
-        
-        
-       
     }
-    //otherwise, drop off our samples
-    else{
-        memcpy(positionTarget,myPos,12);
+    // drop off our samples
+    else {
+        memcpy(positionTarget, myPos, 12);
         if (myPos[2]>.29f){
             positionTarget[2]=.05f;
         }
         else{
-            //maybe take out the mathvecMagnitude expression for codesize
-            scale(positionTarget,(.23f)/mathVecMagnitude(positionTarget,3));//go to a position that is .09 in the same direction at the enemy. In other words, between them and the origin.
+            scale(positionTarget, 0.23f / mathVecMagnitude(positionTarget, 3));
         }
-        zeroVec[2]-=1;
+        zeroVec[2] -= 1;
         api.setAttitudeTarget(zeroVec);
-        //zeroVec[2]=0;//.01f;//Slow down
-        
-        
     }
     
-    //PRINTVEC("myQuatAtt",myQuatAtt);
-    // myQuatAtt[3]*=-1;//inverts rotation - now rotates fundamental basis rotation vector (k-hat) to our basis
-    // api.quat2AttVec(zeroVec,myQuatAtt,usefulVec);
-    // if (drilling){
-    //     api.setAttRateTarget(usefulVec);
-    // }
-    //if our drill breaks or we get a geyser, stop the current drill
-    flocal=game.getFuelRemaining();
-    if (game.getNumSamplesHeld()>1 and ((!(int)((api.getTime()-161)/4)))){// or (flocal<.18f and flocal> .12f))){//at the end of the game, drop off what we have
-        dropping=true;
+    flocal = game.getFuelRemaining();
+	// at the end of the game, drop off what we have
+    if (game.getNumSamplesHeld() > 1 and ((!(int)((api.getTime()-161)/4)))) {
+        dropping = true;
     }
+	
+	// if our drill breaks or we get a geyser, stop the current drill
     if (game.getDrillError() 
     or geyserOnMe 
-    or game.getDrills(mySquare)>MAXDRILLS-1
-    or dropping){
+    or game.getDrills(mySquare) > MAXDRILLS-1
+    or dropping) {
         DEBUG(("Broke"));
-        if (game.getNumSamplesHeld()>3){
-            dropping=true;
+        if (game.getNumSamplesHeld() > 3){
+            dropping = true;
         }
-        drilling=false;
+        drilling = false;
+    }
+
+	// don't drop off with no samples
+	if (!game.getNumSamplesHeld()) {
+        dropping = false;
     }
     
-    // if (flocal<.03f and myVel[2]>0){
-    //     memcpy(positionTarget,myPos,12);
-    //     positionTarget[2]-=1;
-    // }
-    if (!game.getNumSamplesHeld()){//don't drop off with no samples
-        dropping=false;
-    }
-    if (not drilling){//don't drill if we aren't drilling
+	// don't drill if we aren't drilling
+	if (not drilling) {
         game.stopDrill();
     }
 
-    
-    
-	#define destination positionTarget//This (next 20 or so lines) is movement code.
-	//It is fairly strange - we will go over exactly how it works eventually
+	// movement code
+	
+	#define destination positionTarget
     #define fvector usefulVec
     
     #define ACCEL .014f
-    //mathVecSubtract(fvector, destination, myPos, 3);//Gets the vector from us to the target
     mathVecSubtract(fvector, destination, myPos, 3);
-    flocal=0.05f/(.05f+mathVecMagnitude(fvector,3));//Just storing this value as a functional boolean
-    scale(myVel,.2f+flocal);
-    mathVecSubtract(fvector,fvector,myVel,3);
-    scale(fvector,.25f-.09f*flocal);
+    flocal = 0.05f/(.05f + mathVecMagnitude(fvector,3));
+    scale(myVel, 0.2f + flocal);
+    mathVecSubtract(fvector, fvector, myVel, 3);
+    scale(fvector, 0.25f-0.09f*flocal);
     if (geyserOnMe){
-        fvector[2]=0;
-        // flocal=mathVecMagnitude(fvector,3)/15;
-        // fvector[0]/=flocal;
-        // fvector[1]/=flocal;
-        scale(fvector,15/mathVecMagnitude(fvector,3));
-        //fvector[2]=0.05f;
+        fvector[2] = 0;
+        scale(fvector, 15/mathVecMagnitude(fvector,3));
     }
+	// we cannot move too fast when analyzing
     if (!tenFound and game.hasAnalyzer()){
-        fvector[2]=.05f*(.2f-myPos[2]);
+        fvector[2] = 0.05f*(0.2f - myPos[2]);
         mathVecNormalize(fvector,3);
-        //fvector[2]=.1f*(.27f-myPos[2]);
-        //while (mathVecMagnitude(fvector,3)>.039f){
-        scale(fvector,.03f);
-        //}
+        scale(fvector, 0.03f);
     }
+	// cornering
     if (drilling){
-        fvector[2]=.5f*(positionTarget[2]-myPos[2]);
+        fvector[2] = 0.5f*(positionTarget[2] - myPos[2]);
         for (int i=0;i<2;i++){
-            fvector[i]=(((nextSquare[i]>mySquare[i])-(mySquare[i]>nextSquare[i]))*.038f+positionTarget[i]-myPos[i])/(10-4.f*game.getDrills(mySquare));
+            fvector[i] = (((nextSquare[i] > mySquare[i]) - (mySquare[i] > nextSquare[i]))
+			*.038f + positionTarget[i] - myPos[i])/(10 - (4.0f * game.getDrills(mySquare)));
         }
     }
     api.setVelocityTarget(fvector);
 }
 
+/**
+ * @param vec1 - a 3D position vector
+ * @param vec2 - another 3D position vector
+ * @return the euclidean distance between vec1 and vec2
+ */
 float dist(float* vec1, float* vec2) {
     float ansVec[3];
     mathVecSubtract(ansVec, vec1, vec2, 3);
     return mathVecMagnitude(ansVec, 3);
 }
 
+/**
+ * @param coorVec - a square
+ * Changes zeros to ones to reflect that squares cannot
+ * have a row or column equal to zero
+ */
 void fixEdge(int coorVec[2]){
     if (coorVec[1]==0){
         coorVec[1]=1;
@@ -418,6 +438,11 @@ void fixEdge(int coorVec[2]){
     }
 }
 
+/**
+ * @param vec - a length three float array
+ * @param scale - a float coefficient
+ * multiplies each element in vec by scale
+ */
 void scale (float* vec, float scale) {//This function scales a length-3 vector by a coeff.
     for (int i=0; i<3; i++) {
         vec[i] *= scale;
