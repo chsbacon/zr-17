808d127cc3ca68195b3670a6f29ce2fac8679167
jonahweissman <19804455+jonahweissman@users.noreply.github.com>
2017-12-13 20:15:36 -0500

    Re-write center exclusion logic I switched some stuff around to reduce codesize. I'm committing directly to master because this is a very minor change, and I can't imagine someone objecting

--- main.cpp
+++ main.cpp
@@ -138,7 +138,7 @@ void loop() {
         for (int i = -6; i<6; i++){ // checks all clumps of 4 squares (blocks)
         // and sees which is both closest to us and in the center.
             for (int j = -8; j<8; j++){
-                if (i*j !=0 and (i < -3 or i > 2 or j < -3 or j > 2)) {
+                if ((i < -3 or i > 2) and (j < -3 or j > 2)) {
                     
                     // initialize array to store number of squares at each level
                     int heights[4];
